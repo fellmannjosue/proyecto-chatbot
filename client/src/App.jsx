0ce4491840e385ff1997c77f33789da5c472edfa
J-fell2023 <fellmannjosue12@unitec.edu>
2023-02-10 13:39:21 -0600

    report4

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+//import Cookies from 'universal-cookie';
+
+import { ChannelListContainer,ChannelContainer } from './Components';
+
+const apiKey ='yecufc8nrue2';
+
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div>Shool Help Chat</div>
+    <div className='app__wrapper'>
+        <Chat client={client} theme="team light">
+            <ChannelListContainer
+            
+            />
+            <ChannelContainer
+            
+            />
+        </Chat>
+    </div>
   );
 }
 

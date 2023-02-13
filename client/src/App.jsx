6936e6fce52292f363ba7a0666c3f439c707b742
J-fell2023 <fellmannjosue12@unitec.edu>
2023-02-13 11:33:50 -0600

    report 8

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,15 +3,20 @@ import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 
-import { ChannelListContainer, ChannelContainer } from './Components';
+import { ChannelListContainer, ChannelContainer, Auth } from './Components';
 
 import './App.css'
 
 const apiKey = 'yecufc8nrue2';
 
+const autToken = false;
+
 const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
+
+    if(!autToken) return <Auth  />
+
     return (
         <div className='app__wrapper'>
             <Chat client={client} theme="team light">

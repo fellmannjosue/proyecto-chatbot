7c69b20f2266b4e9abf3b097ca8e776aa1fec57b
J-fell2023 <fellmannjosue12@unitec.edu>
2023-02-13 07:59:38 -0600

    report 6

--- client/src/Components/ChannelListContainer.jsx
+++ client/src/Components/ChannelListContainer.jsx
@@ -34,8 +34,41 @@ const ChannelListContainer = () => {
 
             <SideBar />
             <div className='channel-list__list__wrapper'>
-            <CompanyHeader/>
-            <ChannelSearch/>
+                <CompanyHeader />
+                <ChannelSearch />
+                <ChannelList
+                    filters={{}}
+                    channelRenderFilterFn={() => { }}
+                    List={(listProps) => (
+                        <TeamChannelList
+                            {...listProps}
+                            type="team"
+                        />
+                    )}
+                    Preview={(previewprops) => (
+                        <TeamChannelPreview
+                            {...previewprops}
+                            type="team"
+                        />
+                    )}
+                />
+                <ChannelList
+                    filters={{}}
+                    channelRenderFilterFn={() => { }}
+                    List={(listProps) => (
+                        <TeamChannelList
+                            {...listProps}
+                            type="messaging"
+                        />
+                    )}
+                    Preview={(previewprops) => (
+                        <TeamChannelPreview
+                            {...previewprops}
+                            type="messaging"
+                        />
+                    )}
+                />
+
             </div>
         </>
     )

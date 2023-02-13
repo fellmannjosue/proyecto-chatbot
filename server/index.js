907d0ff27d395ddd7ff03b8c68ef98960d4cd1bf
J-fell2023 <fellmannjosue12@unitec.edu>
2023-02-13 12:11:36 -0600

    report 9

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 
+const authRoutes = require("./routes/auth.js");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 require('dotenv').config();
+
 
 
 app.use(cors());
@@ -16,5 +19,6 @@ app.get('/', (req, res) => {
 });
 
 
+app.use('/auth', authRoutes);
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

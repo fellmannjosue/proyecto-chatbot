4104fae8c96135cf53eea891238c99e4c6b2ff8b
J-fell2023 <fellmannjosue12@unitec.edu>
2023-02-14 11:11:53 -0600

    report 10

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,14 +3,24 @@
  const StreamChat = require('stream-chat');
  const crypto = require('crypto');
  
- const signup = (req,res) => {
+ const api_key = process.env.STREAM_API_KEY;
+ const api_secret = process.env.STREAM_API_SECRET;
+ const app_id = process.env.STREAM_APP_ID;
+
+
+ const signup = async (req,res) => {
     try {
         const { fullName, username, password, phoneNumber } = req.body;
 
         const userId = crypto.randomBytes(16).toString('hex');
 
-        const serverClient = connect(api_key, api_secret, app_id)
+        const serverClient = connect(api_key, api_secret, app_id);
+        
+        const hashedPassword = await bcrypt.hash(password,10);
 
+        const token = serverClient.createUserToken(userId);
+
+        res.status(200).json({ token, fullName, username, userId, hashedPassword, phoneNumber})
     } catch (error) {
         console.log(error);
 
